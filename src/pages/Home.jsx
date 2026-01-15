a1c06a288c269970c3e0b29b1896fe6589a7a4eb
di-nosawr <53378234+di-nosawr@users.noreply.github.com>
2026-01-15 13:19:03 -0500

    updated files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,6 @@ import React from "react";
 export default function Home() {
     return (
         <div>
-            <iframe
-                width="1358"
-                height="764"
-                src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1"
-                title="Rick Astley - Never Gonna Give You Up (Official Video) (4K Remaster)"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerPolicy="strict-origin-when-cross-origin"
-                allowFullScreen>
-            </iframe>
-
         </div>
     );
 }
